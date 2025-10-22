504dbb9e90ae065160a0bc39249cb09d635a7c84
richardsonjp <jayaputrarichardson@yahoo.com>
2025-10-22 20:27:40 +0700

    update GA4 for measurement in google analytics

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Analytics as VercelAnalytics } from "@vercel/analytics/react"
-import { GoogleTagManager } from '@next/third-parties/google'
+import Script from "next/script";
 
 interface AnalyticsProps {
     debug?: boolean
@@ -12,5 +12,22 @@ export function AnalyticsVercel({ debug = false }: AnalyticsProps) {
 }
 
 export function AnalyticsGoogle() {
-    return <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID!} />
+    const gaId = process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID!
+
+    return (
+        <>
+            <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${gaId}');
+        `}
+            </Script>
+        </>
+    )
 }
